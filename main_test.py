d6ede3a145a95e5eaa1012c18c9191922a325f5d
aysegmsay <190101039@ogr.atu.edu.tr>
2024-01-09 01:51:10 +0300

    features are tested

--- main_test.py
+++ main_test.py
@@ -1,5 +1,9 @@
 import file_object
-
-F1 = file_object.FileClass("Files/README.md")  # test
+signature_list="Signature_list.csv"
+F1 = file_object.FileClass("Files/test.pdf")  # test
 F1.print_entropy()
+F1.print_signature()
+F1.print_file_size()
+
+
 
