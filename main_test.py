8ad7c7c3e8d5de0be6808d0faab3b2e14e0400c9
gizemduran <gizeemduran@gmail.com>
2024-01-09 20:54:00 +0300

    Hash calculations are tested.

--- main_test.py
+++ main_test.py
@@ -4,5 +4,8 @@ F1 = file_object.FileClass("Files/test.pdf")  # test
 F1.print_entropy()
 F1.print_signature()
 F1.print_file_size()
+F1.print_file_md5()
+F1.print_file_sha1()
+F1.print_file_sha256()
 
 F1.print_file_name()
