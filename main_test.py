44da50671850f094035a528199ddf81fe4376a06
aysegmsay <190101039@ogr.atu.edu.tr>
2024-01-11 19:20:15 +0300

    tested

--- main_test.py
+++ main_test.py
@@ -1,6 +1,7 @@
 import file_object
 signature_list="Signature_list.csv"
 F1 = file_object.FileClass("Files/test.pdf")  # test
+F1.print_file_name()
 F1.print_entropy()
 F1.print_signature()
 F1.print_file_size()
