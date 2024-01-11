13c3edd751db8e901a85c2ff37437ef9898b37dd
aysegmsay <190101039@ogr.atu.edu.tr>
2024-01-11 23:36:02 +0300

    change the features

--- entropy.py
+++ entropy.py
@@ -7,7 +7,7 @@ import hashlib
 folder_path = r"C:\Users\ayseg\OneDrive\Masaüstü\CYBER_DOSYA"
 
 # Create an empty DataFrame to store the results
-columns = ["file_path", "entropy", "hash_md5", "hash_sha1", "hash_sha256", "signature", "encrypted"]
+columns = ["file_name", "entropy", "encrypted"]
 result_df = pd.DataFrame(columns=columns)
 
 # Iterate through files in the folder
@@ -19,21 +19,14 @@ for file_name in os.listdir(folder_path):
         # Use the FileClass to extract entropy, signature, and file string
         try:
             file_instance = FileClass(file_path)
+            file_name=file_instance.get_file_name()
             file_entropy = file_instance.calculate_entropy()
-            file_signature = file_instance.get_signature()
-            hash_md5 = file_instance.calc_file_hash_md5()
-            hash_sha1 = file_instance.calc_file_hash_sha1()
-            hash_sha256 = file_instance.calc_file_hash_sha256()
             encrypted=file_instance.is_encrypted()
 
             # Append results to DataFrame
             result_df = result_df.append({
-                "file_path": file_path,
+                "file_name": file_name,
                 "entropy": file_entropy,
-                "hash_md5": hash_md5,
-                "hash_sha1": hash_sha1,
-                "hash_sha256": hash_sha256,
-                "signature": file_signature,
                 "encrypted":encrypted
             }, ignore_index=True)
         except Exception as e:
