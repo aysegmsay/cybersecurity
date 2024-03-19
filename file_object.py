2cd77885b7e9d4a24c3e7017f77b6ef2d25274eb
aysegmsay <190101039@ogr.atu.edu.tr>
2024-03-20 00:53:25 +0300

    be arranged

--- file_object.py
+++ file_object.py
@@ -122,6 +122,14 @@ class FileClass:
     def print_file_strings(self):
         print("File Strings of the file:", self.get_file_strings())
 
+    def search_from_virus_total(self):
+        url = f"https://www.virustotal.com/api/v3/files/{self.file_md5}"
+        headers = {
+            "accept": "application/json",
+
+        }
+        response = requests.get(url, headers=headers)
+        return response
 
 
 
@@ -135,6 +143,7 @@ class FileClass:
             f"SHA256: {self.calc_file_hash_sha256()[:20]}\n"
             f"MD5: {self.calc_file_hash_md5()[:20]}\n"
             f"file string: {self.get_file_strings()[:10]}\n"
+            f"virustotal result : {self.search_from_virus_total()}\n"
             f"encrypted possibility:{self.is_encrypted()}"
 
         )
